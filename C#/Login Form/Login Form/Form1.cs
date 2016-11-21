9f9d28d250445cba9f2f42bed71ae4be655e5181
Arnlaugur <laugi99@hotmail.com>
2016-11-21 11:51:53 +0000

    Dulkóðun lykilorða

--- C#/Login Form/Login Form/Form1.cs
+++ C#/Login Form/Login Form/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Security.Cryptography;
 namespace Login_Form
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bt_Login_Click(object sender, EventArgs e)
+        {
+            string Notendanafn = tb_Notendanafn.Text;
+            string PasswordA = tb_Password.Text;
+            string PasswordB = Encryption(PasswordA);
+            
+        }
+        static string Encryption(string pass)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] Data = md5.ComputeHash(utf8.GetBytes(pass));
+                return Convert.ToBase64String(Data);
+            }
         }
     }
 }
