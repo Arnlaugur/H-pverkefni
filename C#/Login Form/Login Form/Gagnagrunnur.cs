828b6c8737915419cfaae505925930112c2ad934
Arnlaugur <laugi99@hotmail.com>
2016-11-21 12:11:37 +0000

    Gagnagrunns class

---
+++ C#/Login Form/Login Form/Gagnagrunnur.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Login_Form
+{
+    class Gagnagrunnur
+    {
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        string tengistrengur = null;
+        string fyrirspurn = null;
+
+        MySqlConnection sqltenging;
+        MySqlCommand nySQLskipun;
+        MySqlDataReader sqllesari = null;
+
+        public void TengingVidGagnagrunn()
+        {
+            server = "10.200.10.24";
+            database = "3001992329_Starfsmenn";
+            uid = "3001992329";
+            password = "mypassword";
+            tengistrengur = "Server=" + server + ";userid=" + uid + ";password=" + password + ";database=" + database;
+            sqltenging = new MySqlConnection(tengistrengur);
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                sqltenging.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                sqltenging.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+        public List<string> LesautSQLToflu()
+        {
+            List<string> Faerslur = new List<string>();
+            string lina = null;
+            if (OpenConnection() == true)
+            {
+                fyrirspurn = "SELECT Nafn, Kyn, Aldur FROM hlutaprof2";
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < sqllesari.FieldCount; i++)
+                    {
+                        lina += (sqllesari.GetValue(i).ToString()) + ":";
+                    }
+                    Faerslur.Add(lina);
+                    lina = null;
+
+                }
+                CloseConnection();
+                return Faerslur;
+            }
+            return Faerslur;
+        }
+    }
+}
