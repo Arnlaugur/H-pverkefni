5968f77d1bb0dea2a94b4c874fcbdd7bf742d9bd
Arnlaugur <laugi99@hotmail.com>
2016-12-01 09:43:15 +0000

    Skilaboð og senda í frí komið í gang

--- C#/Login Form/Login Form/Skilaboð.cs
+++ C#/Login Form/Login Form/Skilaboð.cs
@@ -14,14 +14,53 @@ namespace Login_Form
     {
         public Skilaboð()
         {
-            rtb_Skilaboð.Clear();
+            
             InitializeComponent();
-            //rtb_Skilaboð.Text +=;
-        }
+            try
+            {
+                gagnagrunnur.TengingVidGagnagrunn();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            rtb_Skilaboð.Clear();
+            List<string> linur = new List<string>();
+            try
+            {
+                
+                linur = gagnagrunnur.NáÍSkilaboð();
+                foreach (string lin in linur)
+                {
+                string[] linaUrLista = lin.Split('-');
+                string skilaboð = linaUrLista[0];
+                
+                rtb_Skilaboð.Text += skilaboð + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
 
+                MessageBox.Show(ex.ToString());
+            }
+            
+            //List<string>;
+        }
+        Gagnagrunnur gagnagrunnur = new Gagnagrunnur();
         private void bt_fri_Click(object sender, EventArgs e)
         {
-
+            string Notandi = tb_nafn.Text;
+            string email = tb_netfang.Text;
+            string email2 = gagnagrunnur.NafnaCheck(Notandi);
+            if (bt_fri.Text == "senda í frí" && email2 == email)
+            {
+                bt_fri.Text = "Srká úr fríi";
+                gagnagrunnur.FaraÍFrí(Notandi);
+            }
+            else if (bt_fri.Text == "Skrá ír ´fríi")
+            {
+                gagnagrunnur.FaraÚrFríi(Notandi);
+            }
         }
     }
 }
